bc45498eb1561920025553428f93bcfbd9130131
Edison Luciano de Mello <edison.mello@linx.com.br>
2018-07-04 16:55:19 -0300

    Inclusão 2

--- src/app/cliente/cadastro-cliente/cadastro-cliente.component.ts
+++ src/app/cliente/cadastro-cliente/cadastro-cliente.component.ts
@@ -7,9 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastroClienteComponent implements OnInit {
 
+  cliente: string = "Proprietário";
+  contador: number = 0;
+  imagem : string;
+
   constructor() { }
 
   ngOnInit() {
+  let self = this;
+  
+  setTimeout(() => {
+    self.cliente = "Proprietario iniciando...";
+    self.imagem = "assets/Neymar_em_pe.jpg";
+  },1000);
+
+  let interval = setInterval(() => {
+    self.cliente = "Proprietario " + self.contador++;
+    if (self.contador == 5) {
+      clearInterval(interval);
+      self.imagem = "assets/Neymar_deitado.jpg";
+    }
+  },2000);
   }
 
+  levantarNeymar() {
+    this.imagem = "assets/Neymar_em_pe.jpg";
+  }
 }
